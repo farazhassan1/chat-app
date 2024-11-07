b9f1f17f74d15ee6b7b76357761a9194984213c2
Faraz Hassan <farazh583@gmail.com>
2024-11-07 14:12:22 +0500

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js', // Add this if you're using JS files
+        './resources/**/*.vue', // Add this if you're using Vue
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
